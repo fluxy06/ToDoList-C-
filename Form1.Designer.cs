89663dc6e1fe0d03694f6afe8275807f3a9f94d5
Никита <kitsorokin115@gmail.com>
2024-05-21 10:29:37 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.pageSetupDialog2 = new System.Windows.Forms.PageSetupDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.AccessibleName = "";
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(901, 546);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Tag = "";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox6);
+            this.tabPage1.Controls.Add(this.textBox5);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(893, 520);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Мои задачи";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(893, 520);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Создать задачу";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(27, 157);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(819, 81);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(819, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить задачу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(32, 41);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(453, 58);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TabStop = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(32, 128);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(453, 58);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.TabStop = false;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(32, 222);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(453, 58);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.TabStop = false;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(32, 321);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(453, 58);
+            this.textBox5.TabIndex = 3;
+            this.textBox5.TabStop = false;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(32, 418);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(453, 58);
+            this.textBox6.TabIndex = 4;
+            this.textBox6.TabStop = false;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(533, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(303, 58);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Очисить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(533, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(303, 58);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Очисить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(533, 222);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(303, 58);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Очисить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(533, 321);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(303, 58);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Очисить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(533, 418);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(303, 58);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Очисить";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 546);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        private int currentTextBoxIndex = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Click += ClearButton_Click;
+            button3.Click += ClearButton_Click;
+            button4.Click += ClearButton_Click;
+            button5.Click += ClearButton_Click;
+            button6.Click += ClearButton_Click;
+
+            // Назначение обработчика события для кнопки добавления
+            button1.Click += AddButton_Click;
+
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            // Определение, какая кнопка была нажата, и очистка соответствующего TextBox
+            if (sender == button2)
+            {
+                textBox2.Clear();
+            }
+            else if (sender == button3)
+            {
+                textBox3.Clear();
+            }
+            else if (sender == button4)
+            {
+                textBox4.Clear();
+            }
+            else if (sender == button5)
+            {
+                textBox5.Clear();
+            }
+            else if (sender == button6)
+            {
+                textBox6.Clear();
+            }
+
+            ResetTextBoxIndexIfNeeded();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            // Добавление текста из textBox1 в первый доступный пустой TextBox на первой вкладке
+            bool added = false;
+
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                textBox2.Text = textBox1.Text;
+                added = true;
+            }
+            else if (string.IsNullOrEmpty(textBox3.Text))
+            {
+                textBox3.Text = textBox1.Text;
+                added = true;
+            }
+            else if (string.IsNullOrEmpty(textBox4.Text))
+            {
+                textBox4.Text = textBox1.Text;
+                added = true;
+            }
+            else if (string.IsNullOrEmpty(textBox5.Text))
+            {
+                textBox5.Text = textBox1.Text;
+                added = true;
+            }
+            else if (string.IsNullOrEmpty(textBox6.Text))
+            {
+                textBox6.Text = textBox1.Text;
+                added = true;
+            }
+
+            if (added)
+            {
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Все текстовые поля заполнены.");
+            }
+        }
+
+        private void ResetTextBoxIndexIfNeeded()
+        {
+            // Сброс индекса, если все текстовые поля пусты
+            if (string.IsNullOrEmpty(textBox2.Text) &&
+                string.IsNullOrEmpty(textBox3.Text) &&
+                string.IsNullOrEmpty(textBox4.Text) &&
+                string.IsNullOrEmpty(textBox5.Text) &&
+                string.IsNullOrEmpty(textBox6.Text))
+            {
+                // Сбрасываем индекс, чтобы можно было снова добавлять текст с начала
+                currentTextBoxIndex = 0;
+            }
+        }
+
+
+        private void tabPage1_Click(object sender, EventArgs e) //переключатель вкладок
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)//кнопка очистить
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)//кнопка очистить
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)//кнопка очистить
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)//кнопка очистить
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)//кнопка очистить
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//кнопка добавить
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)//textbox с текстом задачи на 2 вкладке
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)//поле с задачей на 1
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)//поле с задачей на 1
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)//поле с задачей на 1
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)//поле с задачей на 1
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)//поле с задачей на 1
+        {
+
+        }
+    }
+}
